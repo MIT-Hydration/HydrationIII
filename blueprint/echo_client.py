8f81e3676e1a56afa19a420504eab9663592aa13
Prakash Manandhar <jsaaymi@gmail.com>
2020-12-30 14:03:36 -0500

    Adding specific IP addresses for test

--- blueprint/echo_client.py
+++ blueprint/echo_client.py
@@ -8,7 +8,8 @@ from .generated import echo_pb2_grpc
 
 
 def run():
-    with grpc.insecure_channel('localhost:50051') as channel:
+    print('On Macbook Client')
+    with grpc.insecure_channel('96.237.232.240:50051') as channel:
         stub = echo_pb2_grpc.EchoStub(channel)
         response = stub.Reply(echo_pb2.EchoRequest(message='Hello World!'))
     print("Echo client received: " + response.message)
--- blueprint/grpc.py
+++ blueprint/grpc.py
@@ -3,4 +3,4 @@ from .generated import echo_pb2_grpc, echo_pb2
 class Echoer(echo_pb2_grpc.EchoServicer):
 
     def Reply(self, request, context):
-        return echo_pb2.EchoReply(message=f'You said: {request.message}')
+        return echo_pb2.EchoReply(message=f'[macbook pro] You said: {request.message}')
