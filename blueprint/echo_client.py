f804645459851a85ce63fb51b6b0823564368cd3
Prakash Manandhar <jsaaymi@gmail.com>
2020-12-30 14:22:40 -0500

    Remove merge marks

--- blueprint/echo_client.py
+++ blueprint/echo_client.py
@@ -8,16 +8,11 @@ from .generated import echo_pb2_grpc
 
 
 def run():
-<<<<<<< HEAD
     print('On Macbook Client')
     with grpc.insecure_channel('96.237.232.240:50051') as channel:
-=======
-    with grpc.insecure_channel('192.168.1.182:50051') as channel:
->>>>>>> 6c88b5c105b30d2fa0e848f071be43e61c49447d
         stub = echo_pb2_grpc.EchoStub(channel)
         response = stub.Reply(echo_pb2.EchoRequest(message='Hello World!'))
     print("Echo client received: " + response.message)
-
 
 if __name__ == '__main__':
     logging.basicConfig()
