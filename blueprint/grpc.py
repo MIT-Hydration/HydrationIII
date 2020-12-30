454d51879b840d50c40d3c175ab36c88a798628e
Prakash Manandhar <jsaaymi@gmail.com>
2020-12-30 14:55:37 -0500

    Replace server to receive from any IP

--- blueprint/grpc.py
+++ blueprint/grpc.py
@@ -3,8 +3,4 @@ from .generated import echo_pb2_grpc, echo_pb2
 class Echoer(echo_pb2_grpc.EchoServicer):
 
     def Reply(self, request, context):
-<<<<<<< HEAD
-        return echo_pb2.EchoReply(message=f'[macbook pro] You said: {request.message}')
-=======
         return echo_pb2.EchoReply(message=f'[Rpi 00] You said: {request.message}')
->>>>>>> 6c88b5c105b30d2fa0e848f071be43e61c49447d
