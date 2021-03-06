6e7c1fbadb7ed6fd52d8c1835a7e8cde6ca6d463
Prakash Manandhar <jsaaymi@gmail.com>
2021-03-06 12:37:19 -0500

    Detects RPi setting and mocks if needed

--- blueprint/hardware.py
+++ blueprint/hardware.py
@@ -3,9 +3,9 @@ hardware.py
 Hardware Interface and Mock Layers for Hydration project.
 """
 
-__author__      = "Prakash Manandhar"
+__author__      = "Prakash Manandhar, Tao Sevigny"
 __copyright__ = "Copyright 2021, Hydration Team"
-__credits__ = ["Prakash Manandhar"]
+__credits__ = ["Prakash Manandhar, Tao Sevigny"]
 __license__ = "Internal"
 __version__ = "1.0.0"
 __maintainer__ = "Prakash Manandhar"
@@ -15,13 +15,17 @@ __status__ = "Production"
 from abc import ABC, abstractmethod
 import time
 import threading
-'''
-from pymodbus.client.sync import ModbusSerialClient
-from pymodbus.payload import BinaryPayloadDecoder
+import configparser
 
-from gpiozero import PWMLED
-from gpiozero import CPUTemperature
-'''
+config = configparser.ConfigParser()
+config.read('config.ini')
+if config.getboolean('Operating System', 'RunningInRPi'):
+    from pymodbus.client.sync import ModbusSerialClient
+    from pymodbus.payload import BinaryPayloadDecoder
+
+    from gpiozero import PWMLED
+    from gpiozero import CPUTemperature
+
 class HardwareFactory:
     
     is_mock = {
@@ -41,7 +45,7 @@ class HardwareFactory:
             return MockDrill()
         else:
             return Drill()
-    
+
 
 class AbstractDrill(ABC):
 
@@ -113,108 +117,108 @@ class MockDrill(AbstractDrill):
     def get_current_mA(self):
         pass
 
-'''   
-class Drill(AbstractDrill):
+if config.getboolean('Operating System', 'RunningInRPi'):
 
-    class DrillThread(threading.Thread):
+    class Drill(AbstractDrill):
 
-        def __init__(self):
-            threading.Thread.__init__(self)
-            self.stopped = True
-            self.sensor_readings = {
-                    "active_power_W": 0.0,
-                    "current_mA": 0.0,
-                    "speed_rpm": 0.0,
-                    "accel_x_g": 0.0,
-                    "accel_y_g": 0.0,
-                    "accel_z_g": 0.0,
-                }
-            self.client = ModbusSerialClient(port='/dev/ttyUSB0', method='rtu', baudrate=9600)
+        class DrillThread(threading.Thread):
 
-        def run(self):
-            self.stopped = False
-            while not self.stopped:
-                loop_start = time.time()
-                result  = self.client.read_holding_registers(address, count,  unit=1)
-                decoder = BinaryPayloadDecoder.fromRegisters(result.registers, 
-                    wordorder = '>', byteorder = '>')
-                current_mA = decoder.decode_32bit_float()
-                power_W = decoder.decode_32bit_float()
-                self.sensor_readings["active_power_W"] = power_W
-                self.sensor_readings["current_mA"] =  current_mA
-                loop_end = time.time()
-                delta_time = loop_end - loop_start
-                if (delta_time < 0.01):
-                    time.sleep(0.01 - delta_time)
+            def __init__(self):
+                threading.Thread.__init__(self)
+                self.stopped = True
+                self.sensor_readings = {
+                        "active_power_W": 0.0,
+                        "current_mA": 0.0,
+                        "speed_rpm": 0.0,
+                        "accel_x_g": 0.0,
+                        "accel_y_g": 0.0,
+                        "accel_z_g": 0.0,
+                    }
+                self.client = ModbusSerialClient(port='/dev/ttyUSB0', method='rtu', baudrate=9600)
+
+            def run(self):
+                self.stopped = False
+                while not self.stopped:
+                    loop_start = time.time()
+                    result  = self.client.read_holding_registers(address, count,  unit=1)
+                    decoder = BinaryPayloadDecoder.fromRegisters(result.registers, 
+                        wordorder = '>', byteorder = '>')
+                    current_mA = decoder.decode_32bit_float()
+                    power_W = decoder.decode_32bit_float()
+                    self.sensor_readings["active_power_W"] = power_W
+                    self.sensor_readings["current_mA"] =  current_mA
+                    loop_end = time.time()
+                    delta_time = loop_end - loop_start
+                    if (delta_time < 0.01):
+                        time.sleep(0.01 - delta_time)
+                    
+            def stop(self):
+                self.stopped = True
+
+        class FileWriterThread(threading.Thread):
+
+            def __init__(self, drill_thread):
+                threading.Thread.__init__(self)
+                self.drill_thread = drill_thread
+                self.stopped = True
                 
-        def stop(self):
-            self.stopped = True
-
-    class FileWriterThread(threading.Thread):
-
-        def __init__(self, drill_thread):
-            threading.Thread.__init__(self)
-            self.drill_thread = drill_thread
-            self.stopped = True
-            
-        def run(self):
-            self.stopped = False
-            fp = open(f"{time_start_s}.csv", "w")
-            fp.write("time_s,")
-            keys = drill_thread.sensor_readings.keys
-            for k in keys:
-                fp.write(f"{k},")
-            fp.write("\n")
-            while not self.stopped:
-                loop_start = time.time()
-                fp.write(f"{loop_start},")
+            def run(self):
+                self.stopped = False
+                fp = open(f"{time_start_s}.csv", "w")
+                fp.write("time_s,")
+                keys = drill_thread.sensor_readings.keys
                 for k in keys:
-                    fp.write(f"{drill_thread.sensor_readings[k]},")
+                    fp.write(f"{k},")
                 fp.write("\n")
-                loop_end = time.time()
-                delta_time = loop_end - loop_start
-                if (delta_time < 0.02):
-                    time.sleep(0.02 - delta_time)
+                while not self.stopped:
+                    loop_start = time.time()
+                    fp.write(f"{loop_start},")
+                    for k in keys:
+                        fp.write(f"{drill_thread.sensor_readings[k]},")
+                    fp.write("\n")
+                    loop_end = time.time()
+                    delta_time = loop_end - loop_start
+                    if (delta_time < 0.02):
+                        time.sleep(0.02 - delta_time)
 
-            fp.close()
+                fp.close()
+                    
+            def stop(self):
+                self.stopped = True
                 
-        def stop(self):
-            self.stopped = True
-            
-    modbus_reg_address = 75
-    modbus_reg_count   = 4
-    motor = PWMLED(12)
-    drill_thread = DrillThread()
-    writer_thread = FileWriterThread(drill_thread)
-    
-    @classmethod
-    def start_sensor_readings(cls):
-        cls.drill_thred.start()
-        cls.writer_thread.start()
+        modbus_reg_address = 75
+        modbus_reg_count   = 4
+        motor = PWMLED(12)
+        drill_thread = DrillThread()
+        writer_thread = FileWriterThread(drill_thread)
+        
+        @classmethod
+        def start_sensor_readings(cls):
+            cls.drill_thred.start()
+            cls.writer_thread.start()
 
-    @classmethod
-    def stop_sensor_readings(cls):
-        cls.drill_thread.stop()
-        cls.writer_thread.stop()
+        @classmethod
+        def stop_sensor_readings(cls):
+            cls.drill_thread.stop()
+            cls.writer_thread.stop()
 
-    @classmethod
-    def set_drill_level(cls, level):
-        cls.motor.value = level
+        @classmethod
+        def set_drill_level(cls, level):
+            cls.motor.value = level
 
-    @classmethod
-    def get_drill_level(cls, level):
-        return cls.motor.value
+        @classmethod
+        def get_drill_level(cls, level):
+            return cls.motor.value
 
-    @classmethod
-    def get_speed_rpm(cls):
-        return cls.drill_thread.sensor_readings["speed_rpm"]
-        pass
+        @classmethod
+        def get_speed_rpm(cls):
+            return cls.drill_thread.sensor_readings["speed_rpm"]
+            pass
 
-    @classmethod
-    def get_active_power_W(cls):
-        return cls.drill_thread.sensor_readings["active_power_W"]
-    
-    @classmethod
-    def get_current_mA(cls):
-        return cls.drill_thread.sensor_reading["current_mA"]
-'''
+        @classmethod
+        def get_active_power_W(cls):
+            return cls.drill_thread.sensor_readings["active_power_W"]
+        
+        @classmethod
+        def get_current_mA(cls):
+            return cls.drill_thread.sensor_reading["current_mA"]
