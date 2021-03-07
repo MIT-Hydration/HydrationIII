475c2b284e0f35a92a0135108cc125beb66ab884
pmanandhar1452 <jsaaymi@gmail.com>
2021-03-07 00:12:02 -0500

    Satisfied with state of position control test

--- ClearPathTestCode/TestHydrationServoMove.py
+++ ClearPathTestCode/TestHydrationServoMove.py
@@ -1,22 +1,34 @@
 import HydrationServo
 import time
+import numpy
+
+def move_to_target_position(target):
+    PID_P = 10000
+    TOLERANCE = 0.001 # m
+    MAX_SPEED = 200 # rpm
+    HydrationServo.set_drill_speed(0)
+    current_position = HydrationServo.get_drill_position()
+    while (abs(current_position - target) > TOLERANCE):
+        speed = PID_P * (target - current_position)
+        if (abs(speed) > MAX_SPEED):
+            speed = numpy.sign(speed)*MAX_SPEED
+        HydrationServo.set_drill_speed(speed)
+        time.sleep(0.1)
+        current_position = HydrationServo.get_drill_position()
+        print (f'Drill Position: {current_position} m')
+    HydrationServo.set_drill_speed(0)
 
 if __name__ == "__main__":
     print ("Init Done!")
-    tolerance = 0.001 # m
     target = -2 * 25.4/1000 # -2 inches to m
-    PID_P = 10
+    PID_P = 1000
     try:
-        HydrationServo.set_drill_speed(0)
-        current_position = HydrationServo.get_drill_position()
-        while (abs(current_position - target) > tolerance):
-            speed = PID_P * (target - current_position) 
-            HydrationServo.set_drill_speed(speed)
-            time.sleep(0.1)
-            current_position = HydrationServo.get_drill_position()
-            print (f'Drill Position: {current_position} m')
-    except:
+        move_to_target_position(0) # home
+        time.sleep(1)
+        move_to_target_position(target)
+    except Exception as e:
         print("Exception!")
+        print(e)
         HydrationServo.set_drill_speed(0)
     HydrationServo.set_drill_speed(0)
 
