5fdde096883346e76b03ccf0ed60704dcadbe4e8
pmanandhar1452 <jsaaymi@gmail.com>
2021-03-06 23:46:34 -0500

    Homing using pythong is working!

--- ClearPathTestCode/TestHydrationServoHoming.py
+++ ClearPathTestCode/TestHydrationServoHoming.py
@@ -8,15 +8,16 @@ if __name__ == "__main__":
         HydrationServo.set_drill_speed(0)
         current_position = HydrationServo.get_drill_position()
         while (abs(current_position) > tolerance):
-            print (f'Drill Position: {current_position} m')
             if (current_position > 0):
                 direction = -1
             else:
                 direction = 1    
-            HydrationServo.set_drill_speed(-10*direction)
+            HydrationServo.set_drill_speed(10*direction)
             time.sleep(0.1)
             current_position = HydrationServo.get_drill_position()
+            print (f'Drill Position: {current_position} m')
     except:
+        print("Exception!")
         HydrationServo.set_drill_speed(0)
     HydrationServo.set_drill_speed(0)
 
