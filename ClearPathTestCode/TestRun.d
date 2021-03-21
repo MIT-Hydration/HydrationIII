47b0a971881c755b27825b4ac4911b940e1784d7
pmanandhar1452 <jsaaymi@gmail.com>
2021-03-21 10:53:49 -0400

    Adding some test code

---
+++ ClearPathTestCode/TestRun.d
@@ -0,0 +1,24 @@
+TestRun.o: TestRun.cpp /home/hydration/ClearPath/inc/inc-pub/pubSysCls.h \
+ /home/hydration/ClearPath/inc/inc-pub/tekTypes.h \
+ /home/hydration/ClearPath/inc/inc-pub/pubCoreRegs.h \
+ /home/hydration/ClearPath/inc/inc-pub/pubRegFldCommon.h \
+ /home/hydration/ClearPath/inc/inc-pub/pubIscRegs.h \
+ /home/hydration/ClearPath/inc/inc-pub/pubMnNetDef.h \
+ /home/hydration/ClearPath/inc/inc-pub/pubMotion.h \
+ /home/hydration/ClearPath/inc/inc-pub/pubMonPort.h \
+ /home/hydration/ClearPath/inc/inc-pub/pubCpmRegs.h \
+ /home/hydration/ClearPath/inc/inc-pub/pubPwrReg.h \
+ /home/hydration/ClearPath/inc/inc-pub/pubNetAPI.h \
+ /home/hydration/ClearPath/inc/inc-pub/mnErrors.h
+/home/hydration/ClearPath/inc/inc-pub/pubSysCls.h:
+/home/hydration/ClearPath/inc/inc-pub/tekTypes.h:
+/home/hydration/ClearPath/inc/inc-pub/pubCoreRegs.h:
+/home/hydration/ClearPath/inc/inc-pub/pubRegFldCommon.h:
+/home/hydration/ClearPath/inc/inc-pub/pubIscRegs.h:
+/home/hydration/ClearPath/inc/inc-pub/pubMnNetDef.h:
+/home/hydration/ClearPath/inc/inc-pub/pubMotion.h:
+/home/hydration/ClearPath/inc/inc-pub/pubMonPort.h:
+/home/hydration/ClearPath/inc/inc-pub/pubCpmRegs.h:
+/home/hydration/ClearPath/inc/inc-pub/pubPwrReg.h:
+/home/hydration/ClearPath/inc/inc-pub/pubNetAPI.h:
+/home/hydration/ClearPath/inc/inc-pub/mnErrors.h:
