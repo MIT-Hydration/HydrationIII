a033028e56ddc6d9b1ed35baba15f2a8d1c90270
oranevalette <80336412+oranevalette@users.noreply.github.com>
2021-04-10 16:41:54 +0200

    Update PumpHardware.py

--- blueprint/PumpHardware.py
+++ blueprint/PumpHardware.py
@@ -1,19 +1,26 @@
-
 from time import sleep  # this lets us have a time delay
-#import RPi.GPIO as GPIO
-
+import time
 from abc import ABC, abstractmethod  # https://docs.python.org/3/library/abc.html
 
+import RPi.GPIO as GPIO
 
-# Abstract Base Class
+PUL = 17  # Stepper Drive Pulses
+# Controller Direction Bit (High for Controller default / LOW to Force a Direction Change).
+DIR = 27
+ENA = 22  # Controller Enable Bit (High to Enable / LOW to Disable).
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(PUL, GPIO.OUT)
+GPIO.setup(DIR, GPIO.OUT)
+GPIO.setup(ENA, GPIO.OUT)
+
+duration = 5000
+delay = 0.0000001
+max_speed = 18
 
 
 class AbstractPump(ABC):
-
-    # class variables ?
-    duration = 5000
-    delay = 0.0000001
-    max_speed= 18
 
     # DIRECTIONS:
     @abstractmethod
@@ -30,11 +37,11 @@ class AbstractPump(ABC):
 
     # SPEED (Liter per Minute):
     @abstractmethod
-    def set_speed_LpM(self, speedLpM_value):
+    def set_speed_lpm(self, speedlpm_value):
         pass
 
     @abstractmethod
-    def get_speed_LpM(self):
+    def get_speed_lpm(self):
         pass
 
     # FILTER CLEANING SEQUENCE:
@@ -48,52 +55,43 @@ class AbstractPump(ABC):
         pass
 
     @abstractmethod
-    def set_speed_PoM(self, speedPoM_value):
+    def set_speed_pom(self, speedpom_value):
         pass
 
     @abstractmethod
-    def get_speed_PoM(self):
+    def get_speed_pom(self):
         pass
 
 
-# Child Class
 class MockPump(AbstractPump):
-    #To directly invoke a method from the abstract class super()._____
-    # init method or "constructor" Dunder method
-    def __init__(self, speedlpm, speedpom, direction):
-        self.speedlpm = speedlpm
-        self.speedpom = speedpom
-        self.direction = direction
-
+    def __init__(self):
+        self.speedlpm = 0
+        self.speedpom = 0
+        self.direction = 0
 
     def run(self):
-        speedvalue= input("What speed ?")
-        print(speedvalue)
-        self.set_speed_LpM(speedvalue)
-        print("speed set : ")
-        print(self.speedlpm)
-
-    # def set_direction_forward(cls):
-    #     cls.get_direction=1
-    #     pass
-    #
-    # def set_direction_reverse(cls):
-    #     cls.direction = 0
-    #     pass
-    #
-    # def get_direction(cls):
-    #     return cls.__direction
-    #     pass
-    #     # SPEED (Liter per Minute):
-    def set_speed_LpM(self, speedLpM_value):
-        self.speedlpm = speedLpM_value
         pass
 
-    def get_speed_LpM(self):
+    def set_direction_forward(self):
+        self.direction = 1
+        pass
+
+    def set_direction_reverse(self):
+        self.direction = 0
+        pass
+
+    def get_direction(self):
+        return self.direction
+
+    def set_speed_lpm(self, speedlpm_value):
+        self.speedlpm = speedlpm_value
+        pass
+
+    def get_speed_lpm(self):
         return self.speedlpm
 
-
     # FILTER CLEANING SEQUENCE:
+
     def cleaning_sequence(self):
         pass
 
@@ -102,97 +100,108 @@ class MockPump(AbstractPump):
     def get_max_speed(self):
         pass
 
-
-    def set_speed_PoM(self, speedPoM_value):
-        self.speedpom = speedPoM_value
+    def set_speed_pom(self, speedpom_value):
+        self.speedpom = speedpom_value
         pass
 
-    def get_speed_PoM(self):
-        return
-        pass
+    def get_speed_pom(self):
+        return self.speedpom
 
 
 class Pump(AbstractPump):
 
-    # init method or "constructor"
     def __init__(self, speedlpm, speedpom, direction):
         self.speedlpm = speedlpm
         self.speedpom = speedpom
         self.direction = direction
 
-    def run(self)
-        GPIO.output(ENA, GPIO.LOW)
-        self.set_direction_forward()
-        # will be a while loop later probably
-        for x in range(duration):
-            GPIO.output(PUL, GPIO.HIGH)
-            sleep(delay)
-            GPIO.output(PUL, GPIO.LOW)
-            sleep(delay)
-        return
+    def run(self):
+        cycles = 20
+        cyclecount = 0
+        while cyclecount < cycles:
+            GPIO.setup(ENA, GPIO.OUT)
+            GPIO.output(ENA, GPIO.LOW)
+            for x in range(duration):
+                GPIO.output(PUL, GPIO.HIGH)
+                sleep(delay)
+                GPIO.output(PUL, GPIO.LOW)
+                sleep(delay)
+        cyclecount = (cyclecount + 1)
+        GPIO.cleanup()
 
+    # The cls parameter is the class object, which allows @classmethod methods to easily instantiate the class, 
+    # regardless of any inheritance going on. 
 
-# The cls parameter is the class object, which allows @classmethod methods to easily instantiate the class, regardless of any inheritance going on.
+    # DIRECTIONS:
 
-# DIRECTIONS:
-    @classmethod
-    def set_direction_forward(cls):
+    def set_direction_forward(self):
         GPIO.output(DIR, GPIO.LOW)
-        cls.direction = 1
+        self.direction = 1
 
-
-    @classmethod
-    def set_direction_reverse(cls):
+    def set_direction_reverse(self):
         GPIO.output(DIR, GPIO.HIGH)
-        cls.direction = 0
+        self.direction = 0
 
-
-    @classmethod
-    def get_direction(cls):
-        return cls.direction
-        pass
+    def get_direction(self):
+        return self.direction
 
         # SPEED (Liter per Minute):
 
-
-    # @classmethod
-    # def set_speed_LpM(cls, speedLpM_value):
-    #     speedLpM = speedLpM_value
-    #     pass
-    #
-    #
-    # @classmethod
-    # def get_speed_LpM(cls):
-    #     return speedLpM
-    #     pass
-
-    #Stop pump 5 sec, reverse pump 5 sec, stop pump 10 sec to let debris settle /
-    #be diluted, normal pump 10 sec, reverse pump 5 sec, stop pump 5 sec, back to normal pumping
-    # FILTER CLEANING SEQUENCE:
-    @classmethod
-    def cleaning_sequence(cls):
+    def set_speed_lpm(self, speedlpm_value):
+        self.speedlpm = speedlpm_value
         pass
 
+    def get_speed_lpm(self):
+        return self.speedlpm
+
+    # Stop pump 5 sec, reverse pump 5 sec, stop pump 10 sec to let debris settle /
+    # be diluted, normal pump 10 sec, reverse pump 5 sec, stop pump 5 sec, back to normal    pumping
+    # FILTER CLEANING SEQUENCE:
+
+    def cleaning_sequence(self):
+        time.sleep(5)
+        start_time = time.time()
+        seconds = 5
+        while True:
+            current_time = time.time()
+            elapsed_time = current_time - start_time
+            self.set_direction_reverse()
+            self.run()
+            if elapsed_time > seconds:
+                print("Finished iterating in:" + str(int(elapsed_time)))
+                break
+        time.sleep(10)
+        start_time = time.time()
+        seconds = 10
+        while True:
+            current_time = time.time()
+            elapsed_time = current_time - start_time
+            self.set_direction_forward()
+            self.run()
+            if elapsed_time > seconds:
+                print("Finished iterating in:" + str(int(elapsed_time)))
+                break
+        seconds = 5
+        while True:
+            current_time = time.time()
+            elapsed_time = current_time - start_time
+            self.set_direction_reverse()
+            self.run()
+            if elapsed_time > seconds:
+                print("Finished iterating in:" + str(int(elapsed_time)))
+                break
+        time.sleep(5)
 
     # MAXSPEED (Percentage of the Max):
-    # @classmethod
-    # def get_max_speed(cls):
-    #     return
-    #     pass
-    #
-    #
-    # @classmethod
-    # def set_speed_PoM(cls, speedPoM_value):
-    #     speedPoM = speedPoM_value
-    #     pass
-    #
-    #
-    # @classmethod
-    # def get_speed_PoM(cls):
-    #     return
-    #     pass
+
+    def get_max_speed(self):
+        pass
+
+    def set_speed_pom(self, speedpom_value):
+        self.speedpom = speedpom_value
+        pass
+
+    def get_speed_pom(self):
+        return self.speedpom
 
 
-
-Pump1=MockPump(1,1,1)
-Pump1.run()
