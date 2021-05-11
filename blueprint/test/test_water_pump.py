642d7092171ce8e724763b2d69cbbfc712a43b42
Prakash Manandhar <jsaaymi@gmail.com>
2021-05-11 09:02:02 -0400

    Change speed test

--- blueprint/test/test_water_pump.py
+++ blueprint/test/test_water_pump.py
@@ -1,4 +1,8 @@
 from ..hardware import HardwareFactory
+import time
 
 if __name__ == "__main__":
     pump = HardwareFactory.getWaterPump()
+    pump.set_speed_pom(50)
+    time.sleep(10)
+    pump.set_speed_pom(0)
