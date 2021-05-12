6b589bbbbf4845e71c4462e49f6bcd954f2a5fd8
Prakash Manandhar <jsaaymi@gmail.com>
2021-05-12 08:10:30 -0400

    3 minute test

--- blueprint/test/test_water_pump.py
+++ blueprint/test/test_water_pump.py
@@ -4,7 +4,7 @@ import time
 if __name__ == "__main__":
     pump = HardwareFactory.getWaterPump()
     pump.set_speed_rpm(50)
-    for i in range(60):
+    for i in range(180):
         print(f'Pump Speed = {pump.get_speed_rpm()} RPM,'\
                ' Flow = {pump.get_flow_rate_lpm()} LPM')
         time.sleep(1)
