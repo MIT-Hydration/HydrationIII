24e1e78ddc148ea316b0db26a5bda2d8725d8ec6
Prakash Manandhar <jsaaymi@gmail.com>
2021-06-11 18:08:45 -0400

    Changed Echo client to use config settings

--- blueprint/echo_client.py
+++ blueprint/echo_client.py
@@ -6,10 +6,18 @@ import grpc
 from .generated import echo_pb2
 from .generated import echo_pb2_grpc
 
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+RPI_IP_ADDRESS_PORT = \
+    f"{config.get('Network', 'MissionControlRPiIPAddress')}:" \
+    f"{config.get('Network', 'GRPCPort')}"
 
 def run():
     print('On Macbook Client')
-    with grpc.insecure_channel('96.237.232.240:50051') as channel:
+    with grpc.insecure_channel(RPI_IP_ADDRESS_PORT) as channel:
         stub = echo_pb2_grpc.EchoStub(channel)
         response = stub.Reply(echo_pb2.EchoRequest(message='Hello World!'))
     print("Echo client received: " + response.message)
