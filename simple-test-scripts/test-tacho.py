0222407dbe11d6b54ef4aa54bdc3ca5b7d31b9c8
sophiey6 <76512204+sophiey6@users.noreply.github.com>
2021-07-06 16:04:43 -0400

    Adding file logging

--- simple-test-scripts/test-tacho.py
+++ simple-test-scripts/test-tacho.py
@@ -31,7 +31,7 @@ class TachoTest:
                     "imu_z_g": 0.0,
                 }
         self.arduino_primed = False
-        
+
     def sensor_values(self):
         if not self.arduino_primed:
             self.port.flush()
@@ -39,23 +39,33 @@ class TachoTest:
         str_rcv = self.port.readline()
         print(str_rcv)
         m = self.pattern.match(str_rcv)
-        if m is not None:    
+        if m is not None:
             self.sensor_readings["arduino_timestamp_ms"] = int(m.group(1))
             self.sensor_readings["tacho_rpm"] =  float(m.group(2))
             self.sensor_readings["imu_x_g"] = float(m.group(3))
             self.sensor_readings["imu_y_g"] = float(m.group(4))
             self.sensor_readings["imu_z_g"] = float(m.group(5))
-            self.arduino_primed = True    
+            self.arduino_primed = True
         return self.sensor_readings
 
 
 if __name__ == "__main__":
-    
+
     tacho = TachoTest()
     time_s = 0
-    while time_s < 60:
+
+    time_start_s = time.time()
+
+    tach_fp = open(f"{time_start_s}_tachmoeter.csv", "w")
+    tach_fp.write("time_s,arduino_timestamp_ms,tacho_rpm,imu_x_g,imu_y_g,imu_z_g")
+
+    while time_s < 600:
         s = tacho.sensor_values()
+        abs_time_s = time.time()
+        tach_fp.write(f"{abs_time_s},")
+        for k in s.sensor_readings:
+            tach_fp.write(f"{k},")
+        tach_fp.write(f"\n")
         print(s)
         time.sleep(0.01)
-        time_s += 0.01 
-
+        time_s += 0.01
