d16835e93a5c73bf90af318cf9442a217743fdfa
Prakash Manandhar <jsaaymi@gmail.com>
2021-07-07 21:15:11 -0400

    Configuring to start server from given config port

--- blueprint/app.py
+++ blueprint/app.py
@@ -5,6 +5,14 @@ from .generated import echo_pb2_grpc, echo_pb2
 from .generated import mission_control_pb2_grpc
 from .mission_control_server import MissionController
 
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+MC_IP_ADDRESS_PORT = f"0.0.0.0:{config.get('Network', 'GRPCActualPort')}"
+
+
 class Echoer(echo_pb2_grpc.EchoServicer):
 
     def Reply(self, request, context):
@@ -17,7 +25,7 @@ class Server:
         server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
         echo_pb2_grpc.add_EchoServicer_to_server(Echoer(), server)
         mission_control_pb2_grpc.add_MissionControlServicer_to_server(MissionController(), server)
-        server.add_insecure_port('0.0.0.0:50051')
+        server.add_insecure_port(MC_IP_ADDRESS_PORT)
         print("Before server.start()")
         server.start()
         print("Mission Control Server Started [OK], Use Ctrl-C to close")
