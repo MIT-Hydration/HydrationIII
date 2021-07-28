0bd2ec770a5b30e952919ef23a6ffa3287b5bbd3
sophiey6 <76512204+sophiey6@users.noreply.github.com>
2021-07-28 11:25:08 -0400

    Update wob_hardware.py

--- blueprint/wob_hardware.py
+++ blueprint/wob_hardware.py
@@ -18,10 +18,9 @@ if config.getboolean('Operating System', 'RunningInRPi'):
 class AbstractWOB(ABC):
 
     @abstractmethod
-    # ruturns a timestamped force reading
+    # returns a timestamped force reading
     def get_force_N(self):
         pass
-
 
 class MockWOBSensor(AbstractWOB):
 
@@ -30,48 +29,53 @@ class MockWOBSensor(AbstractWOB):
 
 if config.getboolean('Operating System', 'RunningInRPi'):
 
-    class WOBSensor(AbstractWOB):
+    class WOBThread(threading.Thread):
 
-        class WOBThread(threading.Thread):
+        def __init__(self): 
+            self.DTPin = config.getint('WOBSensor', 'DTPin')
+            self.SCKPin = config.getint('WOBSensor', 'SCKPin')
+            self.wob_sensor = hx711.HX711(self.DTPin, self.SCKPin)
+            self.referenceWOBUnit = \
+                config.getfloat('WOBSensor', 'CalReading') / config.getfloat('WOBSensor', 'CalNewtons')
+            self.wob_sensor.set_reading_format("MSB", "MSB")
+            self.wob_sensor.set_reference_unit(self.referenceWOBUnit)
+            self.wob_sensor.reset()
+            self.wob_sensor.tare()
+            self.sampling_time = config.getfloat('WOBSensor', 'SamplingTime')
+            self.reading = 0.0
+            self.last_reading = 0.0
 
-            def __init__(self):
-                self.DTPin = config.getint('WOBSensor', 'DTPin')
-                self.SCKPin = config.getint('WOBSensor', 'SCKPin')
-                self.wob_sensor = hx711.HX711(self.DTPin, self.SCKPin)
-                self.referenceWOBUnit = \
-                    config.getfloat('WOBSensor', 'CalReading') / config.getfloat('WOBSensor', 'CalNewtons')
-                self.wob_sensor.set_reading_format("MSB", "MSB")
-                self.wob_sensor.set_reference_unit(self.referenceWOBUnit)
-                self.wob_sensor.reset()
-                self.wob_sensor.tare()
-                self.sampling_time = config.getfloat('WOBSensor', 'SamplingTime')
-                self.reading = 0.0
-                self.last_reading = 0.0
+            threading.Thread.__init__(self)
+            self.stopped = True
 
-                threading.Thread.__init__(self)
-                self.stopped = True
+        def run(self):
+            self.stopped = False
+            
+            while not self.stopped:
+                loop_start = time.time()
+                self.reading = self.wob_sensor.get_weight(self.DTPin)
+                loop_end = time.time()
+                self.last_reading = loop_end
+                delta_time = loop_end - loop_start
+                if (delta_time < self.sampling_time):
+                    time.sleep(self.sampling_time - delta_time)
 
-            def run(self):
-                self.stopped = False
-                while not self.stopped:
-                    loop_start = time.time()
-                    self.reading = self.wob_sensor.get_weight(self.DTPin)
-                    loop_end = time.time()
-                    self.last_reading = loop_end
-                    delta_time = loop_end - loop_start
-                    if (delta_time < self.sampling_time):
-                        time.sleep(self.sampling_time - delta_time)
-
-            def stop(self):
-                self.stopped = True
-
-            def get_force_N(self):
-                return [self.last_reading, self.reading]
-
-        sensor_thread = WOBThread()
-
-        def __init__(self):
-            cls.sensor_thread.start()
+        def stop(self):
+            self.stopped = True
 
         def get_force_N(self):
-            return cls.sensor_thread.get_rpm()
+            return [self.last_reading, self.reading]
+        
+        
+   
+    class WOBSensor(AbstractWOB):
+
+        self.sensor_thread = WOBThread()
+            
+        def __init__(self):
+            self.sensor_thread.start()
+
+        def get_force_N(self):
+            return self.sensor_thread.get_rpm()
+        
+    
