c2549d64aebb01cbf67c88a114623c12df24d779
Prakash Manandhar <jsaaymi@gmail.com>
2021-08-15 18:22:53 -0400

    Add control target up

--- blueprint/test/drill_high_vtest1.py
+++ blueprint/test/drill_high_vtest1.py
@@ -9,7 +9,7 @@ import numpy as np
 Ts = 0.1 # control loop runs every 0.1 second
 Pv = 0.03/500
 Iv = Pv/10
-Pz = 0.03
+Pz = 0.333333
 
 class ControlSystem:
     def __init__(self, Z1target, WOBtarget, WOBmax):
@@ -42,15 +42,6 @@ class ControlSystem:
             
         return V3 
 
-# control targets
-Z1target = -0.7 # m
-#Vmax controlled in CPP code to be VEL_LIM_RPM, 
-# e.g. 600 // (600.0/60.0)*(2.0/1000.0) == 0.02 == 2 cm/sec
-WOBtarget = -100
-WOBmax = 150 # always positive
-
-Ptol = 0.002 # m, position tolerance for stop
-
 Z1_THREAD_PITCH = (2.0/1000.0) # == 2 mm 
 iZ1 = 0
 NUM_RETRIES = 10 # number of times error is cleared in there is a torque limit error
@@ -58,7 +49,7 @@ current_retries = 0
 
 def checkAndClearMotorStatus(rig_h):
     motor_status = rig_h.motorStatus()
-    if (motor_status[0].find("RMSOverloadShutdown")): # no overload
+    if (motor_status[0].find("RMSOverloadShutdown") == -1): # no overload
         current_retries = 0
         return True
     else:
@@ -66,19 +57,28 @@ def checkAndClearMotorStatus(rig_h):
         if (current_retries > NUM_RETRIES):
             return False
         rig_h.clearAlert()
+        time.sleep(1.0)
         return True
 
 if __name__ == "__main__":
     rig_h = HardwareFactory.getRig()
     wob_h = HardwareFactory.getWOBSensor()
     
+    # control targets down
+    Z1target = -0.7 # m
+    #Vmax controlled in CPP code to be VEL_LIM_RPM, 
+    # e.g. 600 // (600.0/60.0)*(2.0/1000.0) == 0.02 == 2 cm/sec
+    WOBtarget = -100
+    WOBmax = 150 # always positive
+
     current_pos = rig.getPosition()
-    z1 = current_pos[0]
+    z1 = current_pos[iZ1]
     z1err = Z1target - z1
    
     control_system = ControlSystem(Z1target, WOBtarget, WOBmax)
-    fp = open(f"Vcommand_{time.time()}.csv", "w")
-    fp.write('time_s,Vcommand_mps,Vcommand_RPM\n')
+    time_start = time.time()
+    fp = open(f"Vcommand_{time_start}.csv", "w")
+    fp.write('UD,time_s,Vcommand_mps,Vcommand_RPM\n')
     while np.abs(z1err) < Ptol:
         loop_start = time.time()
         WOB = wob_h.get_force_N()
@@ -86,15 +86,66 @@ if __name__ == "__main__":
         z1 = current_pos[0]
         Vcommand = control_system.control(z1, WOB)
         Vcommand_rpm = (Vcommand*60)/Z1_THREAD_PITCH
-        fp.write(f'{loop_start},{Vcommand},{Vcommand_rpm}\n')
+        zTorque = rig_h.getTorque(iZ1)
+        fp.write(f'D,{loop_start},{Vcommand},{Vcommand_rpm}\n')
+        print(f"D {loop_start - time_start:0.3f}s: z={z1:0.3f}m, WB={WOB:0.3f}N,"\
+              f" T={zTorque:0.1f}p,"\
+              f" Vc={Vcommand:0.5f}m/s, {Vcommand_rpm:0.1f}RPM")
         
         if (not checkAndClearMotorStatus(rig_h)): # try to clear alerts a number of times
-            break
+            rig_h.set_speed_rpm(iZ1, 0)
+            fp.close()
+            exit(-1)
+
         rig_h.set_speed_rpm(iZ1, Vcommand_rpm)
 
         loop_end = time.time()
         delta_time = loop_end - loop_start
         if (delta_time < Ts):
             time.sleep(Ts - delta_time)
+
+    rig_h.set_speed_rpm(iZ1, 0)
+
+    print("Reached target, moving up")
+    time.sleep(1.0)
+
+    # control targets down
+    Z1target = 0.01 # m
+    #Vmax controlled in CPP code to be VEL_LIM_RPM, 
+    # e.g. 600 // (600.0/60.0)*(2.0/1000.0) == 0.02 == 2 cm/sec
+    WOBtarget = 100
+    WOBmax = 150 # always positive
+
+    current_pos = rig.getPosition()
+    z1 = current_pos[iZ1]
+    z1err = Z1target - z1
+   
+    control_system = ControlSystem(Z1target, WOBtarget, WOBmax)
+    time_start = time.time()
+    while np.abs(z1err) < Ptol:
+        loop_start = time.time()
+        WOB = wob_h.get_force_N()
+        current_pos = rig_h.getPosition()
+        z1 = current_pos[0]
+        Vcommand = control_system.control(z1, WOB)
+        Vcommand_rpm = (Vcommand*60)/Z1_THREAD_PITCH
+        zTorque = rig_h.getTorque(iZ1)
+        fp.write(f'U,{loop_start},{Vcommand},{Vcommand_rpm}\n')
+        print(f"U {loop_start - time_start:0.3f}s: z={z1:0.3f}m, WB={WOB:0.3f}N,"\
+              f" T={zTorque:0.1f}p,"\
+              f" Vc={Vcommand:0.5f}m/s, {Vcommand_rpm:0.1f}RPM")
+        
+        if (not checkAndClearMotorStatus(rig_h)): # try to clear alerts a number of times
+            rig_h.set_speed_rpm(iZ1, 0)
+            fp.close()
+            exit(-1)
+
+        rig_h.set_speed_rpm(iZ1, Vcommand_rpm)
+
+        loop_end = time.time()
+        delta_time = loop_end - loop_start
+        if (delta_time < Ts):
+            time.sleep(Ts - delta_time)
+    
     rig_h.set_speed_rpm(iZ1, 0)
     fp.close()
